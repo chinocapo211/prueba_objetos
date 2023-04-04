f4210fe3e4fe39f98205e0960cb4d3343c95dda7
FELIPE BLAUSTEIN <47807182@ort>
2023-04-04 11:49:39 -0300

    se termino el trabajo y el primer ansioso

--- Program.cs
+++ Program.cs
@@ -8,49 +8,78 @@ opciones(num, listaPersonas);
 
 static void opciones(int num, List<persona> lista)
 {
+    bool exists;
     switch(num)
     {
         case 1:
+        exists = false;
         string DNI = pedirDni(lista);
         string nombre = pedirCadena("Ingrese el nombre de la persona");
         string apellido = pedirCadena("Ingrese el apellido de la persona");
         string mail = pedirMail("Ingrese el mail de la persona");
         DateTime FN = pedirCumple("Ingrese la fecha de nacimiento de la persona");
-        persona pers = new persona(DNI,apellido,nombre,mail,FN);
-        lista.Add(pers);
+        foreach(persona per in lista)
+        {
+            if(per.DNI == DNI)
+            {
+                per.DNI = DNI;
+                per.nombre = nombre;
+                per.email = mail;
+                per.apellido = apellido;
+                per.fechaNacimiento = FN;
+                exists = true;
+            }
+        }
+        if(!exists)
+        {
+            persona persona = new persona(DNI,apellido,nombre,mail,FN);
+            lista.Add(persona);
+        }
+        
         break;
         case 2:
-        int totalEdad,personas;
-        personas = lista.Count();
+        int totalEdad=0,personas=lista.Count(),votantes = 0;
+        foreach(persona pers in lista){
+            totalEdad += pers.obtenerEdad();
+            if(pers.puedeVotar()){
+                votantes++;
+            }
+        }
         Console.WriteLine("Hay " + personas + " personas");
-        totalEdad = datosLista(lista);
+        Console.WriteLine("Hay " + votantes + " personas que pueden votar");
         Console.WriteLine("El promedio de edad es de "+(totalEdad/personas));
         break;
         case 3:
-
+        exists=false;
+        DNI = pedirCadena("Ingrese un DNI para buscar");
+        foreach(persona per in lista){
+            if(per.DNI == DNI){
+                Console.WriteLine("DNI: "+per.DNI);
+                Console.WriteLine("Nombre: "+per.nombre);
+                Console.WriteLine("Apellido: "+per.apellido);
+                Console.WriteLine("Mail: "+per.email);
+                Console.WriteLine("Fecha de nacimiento: "+per.fechaNacimiento);
+                Console.WriteLine("Edad: "+per.obtenerEdad());
+                Console.WriteLine("Puede votar: "+per.puedeVotar());
+                exists=true;
+            }
+        }
+        if(!exists)Console.WriteLine("Error, ese parametro de dni no existe");
         break;
         case 4:
-
+        exists=false;
+        DNI = pedirCadena("Ingrese un DNI a buscar");
+        foreach(persona per in lista){
+            if(per.DNI == DNI)per.email=pedirMail("Ingrese el nuevo mail");
+            exists=true;
+        }
+        if(!exists)Console.WriteLine("Error, ese parametro de dni no existe");
         break;
         case 5:break;
         default:
         Console.WriteLine("Error, no existe esa ocpion. Vuelva a intentar");
         break;
     }
-}
-static int datosLista(List<persona> lista)
-{
-    int devolver = 0,votantes = 0;
-    foreach(persona pers in lista)
-    {
-        devolver += pers.obtenerEdad();
-        if(pers.puedeVotar())
-        {
-            votantes++;
-        }
-    }
-    Console.WriteLine("Hay " + votantes + " personas que pueden votar");
-    return devolver;
 }
 static DateTime pedirCumple(string msj)
 {
@@ -84,20 +113,10 @@ static string pedirDni(List<persona> lista)
 {
     string devolver;
     int desecho;
-    do{
-        devolver = pedirCadena("Ingrese el DNI de la persona (no puede ser igual a uno ya ingresado)");
-        while((devolver.Length < 7 || devolver.Length > 8)|| int.TryParse(devolver, out desecho) == false)
-        {
-            devolver = pedirCadena("Error, el DNI debe tener 8 caracteres y ser solo numerico");
-        }
-    }while(!igualDNI(devolver, lista));
-    return devolver;
-}
-static bool igualDNI(string dni, List<persona> lista)
-{
-    bool devolver = true;
-    foreach(persona pers in lista){
-        if(pers.DNI==dni)devolver = false;
+    devolver = pedirCadena("Ingrese el DNI de la persona");
+    while((devolver.Length < 7 || devolver.Length > 8)|| int.TryParse(devolver, out desecho) == false)
+    {
+        devolver = pedirCadena("Error, el DNI debe tener 8 caracteres y ser solo numérico");
     }
     return devolver;
 }
